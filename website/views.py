8fbea8aa0ff6ef3442b7658a1ed1e9f352756977
ILOMI.LIMOI <chahabdarhossein@gmail.com>
2023-07-19 19:27:35 +0330

    created render html files
    added html files to app

--- website/views.py
+++ website/views.py
@@ -3,12 +3,11 @@ from django.http import HttpResponse,JsonResponse
 # Create your views here.
 
 def home_view(request):
-    return HttpResponse("<h1>HOME</h1>")
+    return render(request, 'website/index.html')
 
 
 def about_view(request):
-    return HttpResponse("<h1>ABOUT PAGE</h1>")
-
+    return render(request, 'website/about.html')
 
 def contact_view(request):
-    return HttpResponse("<h1>contact page</h1>")
+    return render(request, 'website/contact.html')
