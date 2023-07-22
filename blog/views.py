eda248dc48a6cfad12f000b5875b364a713c945e
ILOMI.LIMOI <chahabdarhossein@gmail.com>
2023-07-22 18:09:13 +0330

    making the blog dynamic
    used jinja to make the block dynamic
    and used parameter send from and to htmls

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpRequest,JsonResponse
+from django.shortcuts import render, get_list_or_404
+from django.http import HttpRequest, JsonResponse
+from blog.models import Post
 # Create your views here.
 
+
 def blog_view(request):
-    return render(request, "blog/blog-home.html")
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, "blog/blog-home.html", context)
 
 
-def blog_single(request):
-    return render(request, "blog/blog-single.html")
+def blog_single(request, pid):
+    posts = get_list_or_404(Post, pk=pid)
+    context = {"posts": posts}
+    return render(request, "blog/blog-single.html", context)
+
+
+def test(request, pid):
+    posts = get_list_or_404(Post, pk=pid)
+    context = {"posts": posts}
+    return render(request, "test.html", context)
--- website/urls.py
+++ website/urls.py
@@ -5,7 +5,6 @@ app_name = "website"
 
 urlpatterns = [
     path('', home_view, name="index"),
-    path("about", about_view, name="about"),
-    path("contact", contact_view, name="contact"),
-    path("test", test_view, name="test")
+    path("about/", about_view, name="about"),
+    path("contact/", contact_view, name="contact")
 ]
