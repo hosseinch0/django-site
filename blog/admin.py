4830d74056155e78cc6e1ed19c05706980f0acd8
ILOMI.LIMOI <chahabdarhossein@gmail.com>
2023-07-22 19:45:12 +0330

    added auther
    added author via admin users

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = "EMPTY"
-    list_display = ('titles', 'counted_view', 'status', 'created_date', 'published_date', 'updated_date')
-    list_filter = ('status',)
+    list_display = ('titles', 'author', 'counted_view', 'status', 'created_date', 'published_date', 'updated_date')
+    list_filter = ('status', 'author')
     search_fields = ('titles',)
 admin.site.register(Post,PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -5,13 +5,13 @@ from blog.models import Post
 
 
 def blog_view(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(status=1)
     context = {"posts": posts}
     return render(request, "blog/blog-home.html", context)
 
 
 def blog_single(request, pid):
-    posts = get_list_or_404(Post, pk=pid)
+    posts = get_list_or_404(Post, pk=pid, status = 1)
     context = {"posts": posts}
     return render(request, "blog/blog-single.html", context)
 
